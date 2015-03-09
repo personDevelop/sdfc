5cf745d63b894b906a1ec0da621b61f1e655f130
skiphyuk <liweilong0921@126.com>
2015-03-09 22:40:36 +0800

    增加配置文件
    增加配置文件

--- IM/ChatWebSite/consult.aspx.cs
+++ IM/ChatWebSite/consult.aspx.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FrameCommonEntity;
 
 namespace SignalR
-{
+{ 
     public partial class consult : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
             target.Value = Request.QueryString.Get("code");
             uid.Value = System.Guid.NewGuid().ToString() + "_user";
+            List<ParameterInfo> list = new FrameCommonClient.ParameterInfoClient().GetListByParentId("bdcf2650-c738-4b25-852a-79a615456441");
 
+
+            string html = "";
+            foreach (ParameterInfo item in list)
+            {
+                string type = item.V1Type;
+            }
         }
     }
 }
