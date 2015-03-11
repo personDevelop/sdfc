6068db7182337a4bcda73e8f17a89e4713308ad3
skiphyuk <liweilong0921@126.com>
2015-03-11 17:04:37 +0800

    修改网服在线
    修改网服在线

--- IM/ChatWebSite/UserAvailable.aspx.cs
+++ IM/ChatWebSite/UserAvailable.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using AuthorityEntity.IM;
+using NetworkCommsDotNet;
+using System.Data;
 
 namespace SignalR
 {
@@ -14,18 +17,26 @@ namespace SignalR
             //codingChatHub cc = new codingChatHub();
             Dictionary<string, string> data = new Dictionary<string, string>();
             data = codingChatHub.UserHandler.SeverIds;
+            AuthorityClient.UserInfoClient client=new AuthorityClient.UserInfoClient();
+            DataTable dt=client.GetOnLineWf();
             string result = "";
-            foreach (string str in UserList.getUserId())
+            foreach (DataRow row in dt.Rows)
             {
-               if(data.ContainsKey(str))
-                {
-
-                    result += "<a href='consult.aspx?sid=" + data[str].ToString() + "&code=" + str + "' target='_blank'  style='color:red;'>在线" + str + "</a>";
-                }
-                else {
-                    result += "<a href='#' target='_blank' style='color:blue;'>离线" + str + "</a>";
-                }
+                result += "<a href='consult.aspx?sid=" + row["userid"].ToString() + "&code=" + row["userid"].ToString() + "' target='_blank'  style='color:red;'>在线" + row["NAME"].ToString() + "</a>";
             }
+
+           
+            //foreach (string str in UserList.getUserId())
+            //{
+            //   if(data.ContainsKey(str))
+            //    {
+
+            //        result += "<a href='consult.aspx?sid=" + data[str].ToString() + "&code=" + str + "' target='_blank'  style='color:red;'>在线" + str + "</a>";
+            //    }
+            //    else {
+            //        result += "<a href='#' target='_blank' style='color:blue;'>离线" + str + "</a>";
+            //    }
+            //}
              
 
             Response.Write(result);
